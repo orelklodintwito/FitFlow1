d00737e9de46ca5bce7b6263623608eedbb31d2e
orelklodintwito <o.k.twito@gmail.com>
2026-01-20 15:53:11 +0200

    Setup basic Express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("FitFlow server is running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
