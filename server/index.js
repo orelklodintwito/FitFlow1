e388a2439efd6458d8745e5400d8c89c450bcdd4
orelklodintwito <o.k.twito@gmail.com>
2026-01-20 16:10:57 +0200

    Add nodemon dev script

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
+const app = express();
 app.use(express.json());
 
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB error:", err));
+
 app.get("/", (req, res) => {
-  res.send("FitFlow server is running");
+  res.send("FitFlow server running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
