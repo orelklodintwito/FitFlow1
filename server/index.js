119685f21a4dbd3d93969d925460fe5e06931051
orelklodintwito <o.k.twito@gmail.com>
2026-01-26 18:44:48 +0200

    העברה מלוקלי

--- server/index.js
+++ server/index.js
@@ -18,14 +18,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-// ✅ טיפול מפורש ב־preflight
-app.use(cors());
+
 
 
 /* ===================== DATABASE ===================== */
